b543f6f4345ec01c8fdbad9a5305cb58cd7e521b
Jessie Yim <dancingkktten@gmail.com>
2026-03-17 13:16:31 +0800

    the basic timer that diesnt work - timer - connections - intitial entry point.

--- StarterProjectMultipleViews/ViewControllers/ViewControllerThree.swift
+++ StarterProjectMultipleViews/ViewControllers/ViewControllerThree.swift
@@ -35,29 +35,58 @@ class ViewControllerThree: UIViewController {
     @IBOutlet weak var timerImage: UIImageView!
     
     @IBOutlet weak var timerValue: UILabel!
-    
+    var currentIndex = 0
     // MARK: - Variables and Constants
     
-
+    
+    @State private var time = 00.00
+    @State private var isRunning = false
+    @State private var timer:Timer?
     
     
     
     
+    func startTimer(){
+        guard !isRunning else {return}
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updatetImer), userInfo: nil, repeats: true)
+        isRunning = true
+        //tells the computer that yes the timer is repeating and still counting the timer
+    }
+    func stopTimer(){
+        timer?.invalidate()
+        timer=nil
+        isRunning = false
+        // tells the computer that the timer SHOULD STOP
+    }
     
+    @IBAction func resetTimer(_ sender: Any) {
+        stopTimer()
+        time = 00.00
+        updateTimerLabel()
+    }
     
-    
-    
-    
+    @objc func updatetImer(){
+        time+=0.01
+        updateTimerLabel()
+    }
+    func updateTimerLabel(){
+        timerValue.text=(String(format:"%.2f",time))
+    }
     
     // MARK: - IBActions and Functions
     
-    @IBAction func startOrStop(_ sender: Any) {
+    @IBAction func startOrStop(_ sender: UIButton) {
+        if sender.tag == 1 {
+                stopTimer()
+        }else{
+            startTimer()
+            }
+        }
         
-        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+        }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-}
